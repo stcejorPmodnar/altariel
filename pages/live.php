5fbec67f44a46e1e59af91c0ea0d05f6b72d5f0d
kai.a.vernooy@gmail.com <kai.a.vernooy@gmail.com>
2019-07-11 19:45:05 -0400

    Update <p> with the coordinates

--- pages/live.php
+++ pages/live.php
@@ -18,15 +18,52 @@
             </a>
         </div>
         <div id='bumper'></div>
-
+        <p id=coordinates>test</p>
         <?php 
+
+
             $trans = $_POST['transport'];
             $focus = $_POST['focus'];
 
             echo "<span style='color:white;'>$trans</span><br>";
             echo "<span style='color:white;'>$focus</span>";
-
         ?>
+            <script type="text/javascript">
+
+
+            function getCoords(){
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    document.getElementById('coordinates').innerHTML = position.coords.latitude + ',' + position.coords.longitude;
+                });
+            }
+            
+            getCoords()
+
+            // function getLocation() {
+            //     if (navigator.geolocation) {
+            //         navigator.geolocation.getCurrentPosition(showPosition);
+            //     } else {
+            //         x.innerHTML = "Geolocation is not supported by this browser.";
+            //     }
+            // }
+
+            // function showPosition(position) {
+            //     var coords = position.coords.latitude + ',' + position.coords.longitude; 
+            //     return coords
+            // }
+            // console.log(getLocation());
+            // function coords() {
+            //     if(navigator.geolocation) {
+            //         navigator.geolocation.getCurrentPosition(position => {
+            //             console.log("Latitude:" + position.coords.latitude + "\nLongitude:" + position.coords.longitude)
+            //         })
+            //     }
+            // }
+            // for (let i = 0; i < 5; i++) {
+            //     document.getElementById('coordinates').innerHTML = coords();
+            // }
+
+        </script>
 
     </body>
 </html>
