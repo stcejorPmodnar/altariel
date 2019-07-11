27e7e8cc2f74d96d85859a32a9d9143280e371fd
kai.a.vernooy@gmail.com <kai.a.vernooy@gmail.com>
2019-07-11 17:24:18 -0400

    Added php form for live data display

---
+++ pages/live.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fitness App | Your Exercise</title>
+        <link rel="stylesheet" type="text/css" href="../stylesheets/main.css">
+        <link rel="stylesheet" type="text/css" href="../stylesheets/exercise.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="navbar">
+            <h2 id="navbar-title">Fitness App</h2>
+            <a href="../index.html" class="navbar-link-text">
+                <h4>Home</h4>
+            </a>
+            <a href="exercise.html" class="navbar-link-text">
+                <h4>Exercise</h4>
+            </a>
+        </div>
+        <div id='bumper'></div>
+    </body>
+</html>
